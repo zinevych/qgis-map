5b00963e4a5b588fcfee143ce93b2eef2b428bb8
Oleksandr Zinevych <aleksanderzinewicz@gmail.com>
2019-02-21 00:47:58 +0200

    wildfire maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const google: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'medium-maps';
+  title: string = 'medium articles';
+  lat: number = 36.778259;
+  lng: number = -119.417931;
+  zoom: number = 6;
+  mapInstance: any = null;
+
+  setMapInstance (event: any): void {
+    this.mapInstance = event;
+
+    this.mapInstance.overlayMapTypes.clear();
+    this.mapInstance.overlayMapTypes.insertAt(0, this.getLayer());
+  }
+
+  getLayer() {
+    let TILE_URL = 'http://127.0.0.1:8080/wildfire/{z}/{x}/{y}.png';
+    let layerID = 'wildfire_layer';
+
+    const layer = new google.maps.ImageMapType({
+      name: layerID,
+      getTileUrl: function(coord, zoom) {
+          let url = TILE_URL
+              .replace('{x}', coord.x)
+              .replace('{y}', coord.y)
+              .replace('{z}', zoom);
+          return url;
+      },
+      tileSize: new google.maps.Size(256, 256)
+  });
+  return layer;
+  }
 }
